2562a9733598f102c6cd1caa14cf5d791a48161e
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-23 22:55:55 +0500

    add: value function

--- Объекты/22.js
+++ Объекты/22.js
@@ -1,7 +1,2 @@
-let ask = (question, yes, no) => confitm(question) ? yes() : no();
-
-ask(
-    'Вы согласны?',
-    () =>{ alert("YES"); },
-    () => { alert("NO"); }
-)
+let ask = (question,yea,no)=> question?yea:no;
+alert(ask (confirm("Вы согласны?"),"Вы согласились.","Вы отменили выполнение."));

6b251df7fe6d24227864b02acc4ba99a142ccea2
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-25 01:18:59 +0500

    add: recursion

--- Массивы/10.js
+++ Массивы/10.js
@@ -1,23 +1,11 @@
-let count = 0;
-let sum = 0;
-function cc(card) {
-// Only change code below this line
-
-    if (card === 2 || card === 3 || card === 4 || card === 5 || card === 6){
-        count++;
-        //sum += count;
-    } else if (card === 7 || card === 8 || card === 9){
-        count--;
-        //sum -= count;
-    } else if (card === 10 || card === 'J' || card === 'Q' || card === 'K' || card === 'A') {
-        count--;
-        //sum -= count;
+function rangeOfNumbers(startNum, endNum) {
+    if (startNum === endNum){
+        return [startNum];
+    } else {
+        const rangeNum = rangeOfNumbers(startNum, endNum - 1);
+        rangeNum.push(endNum);
+        return rangeNum;
     }
+};
 
-    //return console.log(count)
-   return (count > 0) ? console.log(count + " Bet") : console.log(count + " Hold ");
-    // return "Change Me";
-    // Only change code above this line
-}
-
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+console.log(rangeOfNumbers(4, 9))

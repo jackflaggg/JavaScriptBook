551a0e59cccbafcaa3a753719db791ac7298729c
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-25 00:52:51 +0500

    add: massiv and this

--- Массивы/10.js
+++ Массивы/10.js
@@ -1,11 +1,23 @@
-function sumAll(...args){
-    let sum = 0;
+let count = 0;
+let sum = 0;
+function cc(card) {
+// Only change code below this line
 
-    for (const arg of args) sum += arg
+    if (card === 2 || card === 3 || card === 4 || card === 5 || card === 6){
+        count++;
+        //sum += count;
+    } else if (card === 7 || card === 8 || card === 9){
+        count--;
+        //sum -= count;
+    } else if (card === 10 || card === 'J' || card === 'Q' || card === 'K' || card === 'A') {
+        count--;
+        //sum -= count;
+    }
 
-    return sum;
+    //return console.log(count)
+   return (count > 0) ? console.log(count + " Bet") : console.log(count + " Hold ");
+    // return "Change Me";
+    // Only change code above this line
 }
 
-console.log( sumAll(1) ); // 1
-console.log( sumAll(1, 2) ); // 3
-console.log( sumAll(1, 2, 3) ); // 6
+cc(2); cc(3); cc(7); cc('K'); cc('A');

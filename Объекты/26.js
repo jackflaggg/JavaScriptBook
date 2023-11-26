1e7b192d24dace0276e82e3d3ca266bda29969cf
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-26 21:16:27 +0500

    add: object

--- Объекты/26.js
+++ Объекты/26.js
@@ -6,8 +6,15 @@ let currentUser = {
 function isEmpty(obj){
     for (const objKey in obj) {
         return true;
-    };
+    }
     return false;
 };
+
+function isEmptyTwo(obj){
+    for (const objKey in obj) {
+        return obj.hasOwnProperty(objKey);
+    }
+    return false;
+}
 
 console.log(isEmpty(currentUser))

563182f43507604c95bf5e661a4d48f9286faf52
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-11-27 22:46:29 +0500

    add year

---
+++ Массивы/13.js
@@ -0,0 +1,13 @@
+function ZeroArr(m, n){
+    let newArrOne = [];
+    let newArrTwo = [];
+    for (let i = 0; i < m.length; i++) {
+        for (let j = 0; j < n.length; j++) {
+            newArr.push(0);
+        }
+        newArrTwo.push(newArr)
+    }
+    return console.log(newArrTwo);
+}
+
+const result = ZeroArr(3, 3);

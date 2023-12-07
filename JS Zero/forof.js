f9a9f00d29011efa6d5073435654dfed9d965a46
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-12-07 13:14:01 +0500

    add: for of

--- JS Zero/forof.js
+++ JS Zero/forof.js
@@ -1,5 +1,10 @@
 const myString = 'Rasul';
+const myArray = ['first', 'second', 'elem']
 
 for (const myStringElement of myString) {
     console.log(myStringElement, myString)
+}
+
+for (const string of myArray) {
+    console.log(string)
 }

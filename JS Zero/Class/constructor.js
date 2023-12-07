1b1427e49e998e25ef373cc1778e5dd33dbc31c9
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-12-07 16:28:36 +0500

    add: class

--- JS Zero/Class/constructor.js
+++ JS Zero/Class/constructor.js
@@ -1,12 +1,16 @@
 class NewComment {
-    constructor(text) {
+    constructor(text, vot) {
         this.text = text;
         this.vot = vot;
     }
 
     upvot(){
-        this.vot++;
+        this.vot += 1;
     }
 }
 
-const firstComment = new NewComment('hui')
+const firstComment = new NewComment('hui');
+console.log(firstComment);
+
+console.log(firstComment instanceof NewComment)
+console.log(firstComment instanceof Number)
